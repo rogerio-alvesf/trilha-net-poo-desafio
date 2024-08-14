d04cc4b71909f93c6366f4e01f68b58cf0d97d9f
rogerio-alvesf <rogeriobusiness@protonmail.com>
2024-08-14 10:36:32 -0300

    Fix: Implementando correções do sistema.

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,13 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia(string numero,
+                       string modelo,
+                       string imei,
+                       int memoria) : Smartphone(numero, modelo, imei, memoria)
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo '{nomeApp}' no Nokia");
+        }
     }
 }
--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,23 +3,26 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private readonly string _modelo;
+        private readonly string _imei;
+        private readonly int _memoria;
 
-        public Smartphone(string numero)
+        public Smartphone(string numero,
+                          string modelo,
+                          string imei,
+                          int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            _modelo = modelo;
+            _imei = imei;
+            _memoria = memoria;
         }
 
         public void Ligar()
-        {
-            Console.WriteLine("Ligando...");
-        }
+            => Console.WriteLine("Ligando...");
 
         public void ReceberLigacao()
-        {
-            Console.WriteLine("Recebendo ligação...");
-        }
+            => Console.WriteLine("Recebendo ligação...");
 
         public abstract void InstalarAplicativo(string nomeApp);
     }
